70478bc9ee8a150419182dfa581433548b259e22
nigtas <nigtas@gmail.com>
2015-04-26 15:43:57 +0300

    segments functions included

--- src/CommandsInterpretator.java
+++ src/CommandsInterpretator.java
@@ -30,13 +30,13 @@ public class CommandsInterpretator {
 			case "0D" : div();
 						RealMachine.getInstance().setTM(RealMachine.getInstance().decReg(RealMachine.getInstance().getTM()));
 						break;
-			case "CD" : cds(command.substring(2, 3));
+			case "CD" : cds(command.substring(2, 4));
 						RealMachine.getInstance().setTM(RealMachine.getInstance().decReg(RealMachine.getInstance().getTM()));
 						break;
-			case "CC" : ccs(command.substring(2, 3));
+			case "CC" : ccs(command.substring(2, 4));
 						RealMachine.getInstance().setTM(RealMachine.getInstance().decReg(RealMachine.getInstance().getTM()));
 						break;
-			case "CS" : css(command.substring(2, 3));
+			case "CS" : css(command.substring(2, 4));
 						RealMachine.getInstance().setTM(RealMachine.getInstance().decReg(RealMachine.getInstance().getTM()));
 						break;
 			case "JP" : jp(command.substring(2, 4));
@@ -73,17 +73,15 @@ public class CommandsInterpretator {
 
 	/*commands functions*/
 	public void ld(String elements) {
+		//TODO check address
 		int ds = Utilities.getInstance().charToInt(RealMachine.getInstance().getDS(), 16);
 		int ptr = Utilities.getInstance().charToInt(RealMachine.getInstance().getPTR(), 16);
-		// System.out.println("ds " + ds + " ptr " + ptr);
 		int block = Utilities.getInstance().charToInt(RealMachine.getInstance().getRAM().getWord(ptr, ds), 16);
 		int place = Integer.parseInt(elements, 16);
-		// System.out.println("LOAD block: " + block + " place: " + place);
 		char[] valueFromMemory = RealMachine.getInstance().getRAM().getWord(block, place);			
 		int ss = Utilities.charToInt(RealMachine.getInstance().getSS(), 16);
 		int stackBlock = Utilities.charToInt(RealMachine.getInstance().getRAM().getWord(ptr, ss), 16);
 		int stackTop = Utilities.charToInt(RealMachine.getInstance().getESP(), 16);
-		// System.out.println("place " + stackBlock + " top " + stackTop + " value " + new String(valueFromMemory));
 		if(RealMachine.getInstance().decESP()){
 			RealMachine.getInstance().getRAM().setWord(stackBlock, stackTop, valueFromMemory);
 			GraphicalUserInterface.getInstance().updateRAMCell(stackBlock * 256 + stackTop, new String(RealMachine.getInstance().getRAM().getWord(stackBlock, stackTop)));
@@ -94,6 +92,7 @@ public class CommandsInterpretator {
 	}
 
 	public void pt(String elements) {
+		//TODO check address
 		int ds = Utilities.getInstance().charToInt(RealMachine.getInstance().getDS(), 16);
 		int ptr = Utilities.getInstance().charToInt(RealMachine.getInstance().getPTR(), 16);
 		int block = Utilities.getInstance().charToInt(RealMachine.getInstance().getRAM().getWord(ptr, ds), 16);
@@ -130,7 +129,6 @@ public class CommandsInterpretator {
 				//TODO check if not above 255
 				int sum = Utilities.charToInt(firstValueFromStack, 16) + Utilities.charToInt(secondValueFromStack, 16);
 				char[] valueToAdd = (Integer.toHexString(sum)).toCharArray();
-				// System.out.println("value to add " + new String(valueToAdd) + " stackblock " + stackBlock + " stack top " + stackTop);
 				RealMachine.getInstance().getRAM().setWord(stackBlock, stackTop, valueToAdd);
 				GraphicalUserInterface.getInstance().updateRAMCell(stackBlock * 256 + stackTop, new String(RealMachine.getInstance().getRAM().getWord(stackBlock, stackTop)));	
 			}
@@ -221,12 +219,10 @@ public class CommandsInterpretator {
 			}
 			int stackTop = Utilities.charToInt(RealMachine.getInstance().getESP(), 16);
 			if (RealMachine.getInstance().decESP()) {
-				// System.out.println("first value " + Utilities.charToInt(secondValueFromStack, 16) + " second " + Utilities.charToInt(firstValueFromStack, 16));
 				int div = Utilities.charToInt(secondValueFromStack, 16) / Utilities.charToInt(firstValueFromStack, 16);
 				int mod = Utilities.charToInt(secondValueFromStack, 16) % Utilities.charToInt(firstValueFromStack, 16);
 				char[] valueToAddDiv = (Integer.toHexString(div)).toCharArray();
 				char[] valueToAddMod = (Integer.toHexString(mod)).toCharArray();
-				// System.out.println("DIV " + new String(valueToAddDiv) + " MOD " + new String(valueToAddMod));
 				RealMachine.getInstance().getRAM().setWord(stackBlock, stackTop, valueToAddDiv);
 				GraphicalUserInterface.getInstance().updateRAMCell(stackBlock * 256 + stackTop, new String(RealMachine.getInstance().getRAM().getWord(stackBlock, stackTop)));	
 				stackTop = Utilities.charToInt(RealMachine.getInstance().getESP(), 16);
@@ -245,15 +241,60 @@ public class CommandsInterpretator {
 	}
 
 	public void cds(String elements) {
-		RealMachine.getInstance().setPI(new char[] {'0', '8'});
+		int place = Integer.parseInt(elements, 16);
+		if(place < 256) {
+			RealMachine.getInstance().setDS(elements.toCharArray());
+			int ptr = Utilities.getInstance().charToInt(RealMachine.getInstance().getPTR(), 16);
+			char[] block = RealMachine.getInstance().getRAM().getWord(ptr, place);
+			if(block == new char[] {'-', '-', '-', '-'}) {
+
+			}
+			else {
+
+			}
+			RealMachine.getInstance().setPI(new char[] {'0', '8'});
+		}
+		else {
+			RealMachine.getInstance().setPI(new char[] {'0', '3'});
+		}
 	}
 
 	public void ccs(String elements) {
-		RealMachine.getInstance().setPI(new char[] {'0', '9'});
+		int place = Integer.parseInt(elements, 16);
+		if(place < 256) {
+			RealMachine.getInstance().setDS(elements.toCharArray());
+			int ptr = Utilities.getInstance().charToInt(RealMachine.getInstance().getPTR(), 16);
+			char[] block = RealMachine.getInstance().getRAM().getWord(ptr, place);
+			if(block == new char[] {'-', '-', '-', '-'}) {
+
+			}
+			else {
+				
+			}
+			RealMachine.getInstance().setPI(new char[] {'0', '9'});
+		}
+		else {
+			RealMachine.getInstance().setPI(new char[] {'0', '3'});
+		}
 	}
 
 	public void css(String elements) {
-		RealMachine.getInstance().setPI(new char[] {'0', 'A'});
+		int place = Integer.parseInt(elements, 16);
+		if(place < 256) {
+			RealMachine.getInstance().setDS(elements.toCharArray());
+			int ptr = Utilities.getInstance().charToInt(RealMachine.getInstance().getPTR(), 16);
+			char[] block = RealMachine.getInstance().getRAM().getWord(ptr, place);
+			if(block == new char[] {'-', '-', '-', '-'}) {
+
+			}
+			else {
+				
+			}
+			RealMachine.getInstance().setPI(new char[] {'0', 'A'});
+		}
+		else {
+			RealMachine.getInstance().setPI(new char[] {'0', '3'});
+		}
 	}
 
 	public void jp(String elements) {
